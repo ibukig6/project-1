471ae13bdbed3c66d8ecb5659ad8c28181436ed5
亞亞之星 <python41141218@gmail.com>
2023-03-30 16:22:25 +0800

    third

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def hello_python():
     return "<p>Hello, python!</p>"
 
+@app.route("/name/<name>")
+def name(name):
+    print('Type:',type(name))
+    return name
+
+@app.route("/number/<int:number>")
+def number(number):
+    print('Type:',type(number))
+    return f"{number}"
+
+@app.route("/page")
+def email():
+    email = request.args.get("email")
+    password = request.args.get("password")
+    return f"{email},{password}"
+
 @app.route("/index")
 def index():
     return render_template("index.html")
