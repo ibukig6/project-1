186f4edc6f3513c765771761435fdb43fec87897
盧重伍 <python41141218@gmail.com>
2023-04-20 16:14:54 +0800

    2023.4.20

--- app.py
+++ app.py
@@ -17,15 +17,35 @@ def number(number):
     return f"{number}"
 
 @app.route("/page")
-def email():
-    email = request.args.get("email")
-    password = request.args.get("password")
-    return f"{email},{password}"
+def page():
+    x="1234"
+    dict1 = {"abc":1324,"name":"tom"}
+    return render_template("page.html",a=x,b=dict1)
+#def email():
+#    email = request.args.get("email")
+#    password = request.args.get("password")
+#    return f"{email},{password}"
 #http://127.0.0.1:5000/page?email=13&password=12
 
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        name =request.form.get("account")
+        password=request.form.get("password")
+        if name == "123" and password =="123":
+            type ="成功"
+            return render_template("page2.html",id=name,ps=password,type=type)
+        else:
+            type="登入失敗"
+            return render_template("login.html",type=type)
+    else:
+        return render_template("login.html")
+
+
 
 if __name__ =="__main__":
     app.run(debug=True)
